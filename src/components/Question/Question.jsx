e74d2c7532bdafd6a18edb29d510b5f1381bc5cc
vickyadri29 <vickyadri290501@gmail.com>
2022-07-16 23:25:10 +0800

    feat: add score

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 
-const Question = ({handleForAnswer,
+const Question = ({handleForAnswer, handleForNext, showAnswers,
   data: { category, difficulty, question, correct_answer, answers }
 }) => { 
   return (
@@ -13,14 +13,20 @@ const Question = ({handleForAnswer,
       </div>
       <div className="btn-quiz">
         {answers.map((answer, i) => {
+          const checkAnswers = showAnswers ? (
+            answer === correct_answer ? "green-btn": "red-btn"
+          ) : " ";
           return (
             <button onClick={() => handleForAnswer(answer)}
-              className="btn-answer"
+              className={`btn-answer ${checkAnswers}`}
               dangerouslySetInnerHTML={{ __html: answer }}
             />
           );
         })}
       </div>
+      {showAnswers && (
+        <button onClick={handleForNext} className="next-btn">Next Question</button>
+      )}
     </div>
   );
 };
--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -9,6 +9,7 @@ const Quiz = () => {
   const [questions, setQuestions] = useState([]);
   const [index, setIndex] = useState(0);
   const [point, setPoint] = useState(0);
+  const [showAnswers, setShowAnswers] = useState(false);
 
   useEffect(() => {
     axios
@@ -27,19 +28,31 @@ const Quiz = () => {
   }, []);
 
   const handleForAnswer = (answer) => {
-    if (answer === questions[index].correct_answer) {
-      setPoint(point + 10);
+    if (!showAnswers) {
+      if (answer === questions[index].correct_answer) {
+        setPoint(point + 10);
+      }
     }
 
+    setShowAnswers(true);
+  };
+
+  const handleForNext = () => {
     setIndex(index + 1);
+    setShowAnswers(false);
   };
 
   return questions.length > 0 ? (
     <div>
       {index >= questions.length ? (
         <h3>Selesai! Poin anda {point}</h3>
-      ): (
-        <Question data={questions[index]} handleForAnswer={handleForAnswer} />
+      ) : (
+        <Question
+          data={questions[index]}
+          handleForAnswer={handleForAnswer}
+          showAnswers={showAnswers}
+          handleForNext={handleForNext}
+        />
       )}
     </div>
   ) : (
